96a2ead4fd6f74de8da403af698cf4ebab77dc50
hucilluc <katehenderson@gmx.co.uk>
2021-01-27 11:29:59 +0000

    caves loaded and glacial cavern selected

---
+++ Cave with new lighting/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/Kate/Desktop/Github/cave-with-new-lighting/Cave with new lighting/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1659429255819297599.json
